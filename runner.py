725aee59fb8cce8de84aee17fb5b5714929e86e6
whirls ⚡️ <whirlygig@users.noreply.github.com>
2017-05-10 11:04:14 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -4,5 +4,5 @@ import sys
 for j in range(1, 100):  
     filename = str(j * 100) + 'songs.txt'
     filename.strip('\'')
-    torun = "python3.6 music_catalog.py " + filename
-    os.system(torun)
+    to_run = "python3.6 music_catalog.py " + filename
+    os.system(to_run)
